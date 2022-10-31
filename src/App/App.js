185caf08c3ded10dac1d96d0322b03f6b787b46e
Hazel Pablo <hazel.pablo90@yahoo.com>
2022-10-31 10:22:43 -0600

    add reservation form

--- src/App/App.js
+++ src/App/App.js
@@ -2,41 +2,44 @@ import React, { Component } from 'react';
 import './App.css';
 import { fetchAllReservations } from '../api';
 import Reservations from '../components/Reservations/Reservations';
+import Form from '../components/Form/Form';
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      reservations: null
-    }
+      reservations: null,
+    };
   }
-  componentDidMount = async () =>{
+  componentDidMount = async () => {
     try {
-      const reservationList = await fetchAllReservations()
-      const data = await reservationList.json()
+      const reservationList = await fetchAllReservations();
+      const data = await reservationList.json();
       this.setState({ reservations: data });
-
     } catch {
       this.setState({
-        error: 'error getting reservations'
-      })
-      console.log(this.state.error)
+        error: "error getting reservations",
+      });
+      console.log(this.state.error);
     }
-  }
-  render() {   
- if (!this.state.reservations) {
-   return <h2 className="error-message">{this.state.error}</h2>;
- }
+  };
+  addReservation = (newReservation) => {
+    this.setState({ reservations: [...this.state.reservations, newReservation] });
+  };
+  render() {
+    if (!this.state.reservations) {
+      return <h2 className="error-message">{this.state.error}</h2>;
+    }
     return (
       <div className="App">
-        <h1 className='app-title'>Turing Cafe Reservations</h1>
-        <div className='resy-form'>
-
+        <h1 className="app-title">Turing Cafe Reservations</h1>
+        <div className="resy-form">
+          <Form addReservation={this.addReservation}/>
         </div>
-        <div className='resy-container'>
-          <Reservations reservations={this.state.reservations}/>
+        <div className="resy-container">
+          <Reservations reservations={this.state.reservations} />
         </div>
       </div>
-    )
+    );
   }
 }
 
---
+++ src/components/Form/Form.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      time: "",
+      number: "",
+      date: "",
+    };
+  }
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  submitReservation = (event) => {
+    event.preventDefault(); 
+    const newReservation = {
+      id: Date.now(),
+      ...this.state, 
+    };
+    this.props.addReservation(newReservation); 
+    this.clearInputs(); 
+  };
+
+  clearInputs = () => {
+    this.setState({ name: "", time: "", number: "", date: "" });
+  };
+  render() {
+    return (
+      <form className="reservation-form">
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={this.state.name}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <input
+          type="text"
+          placeholder="Date (mm/dd)"
+          name="date"
+          value={this.state.date}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <input
+          type="text"
+          placeholder="Time"
+          name="time"
+          value={this.state.time}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <input
+          type="text"
+          placeholder="Number of guests"
+          name="number"
+          value={this.state.number}
+          onChange={(event) => this.handleChange(event)}
+        />
+        <button onClick={(event) => this.submitReservation(event)}>
+          Make Reservation
+        </button>
+      </form>
+    );
+  }
+}
+
+export default Form;
